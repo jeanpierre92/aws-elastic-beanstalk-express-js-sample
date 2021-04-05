2cf3f3b897d531fec35b050f842bdd1e6c252259
jeanpierre92 <jpbalster392@msn.com>
2021-04-05 21:47:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!. Nieuwe app website :D'));
+app.get('/', (req, res) => res.send('Hello World!. Nieuwe app website :D. Nu met manual code check before deploying live!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
